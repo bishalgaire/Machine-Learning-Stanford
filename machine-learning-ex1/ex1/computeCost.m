11dded830f5c6ec9661300f1d7ea39cbfac415d4
Bishal Gaire <bgaire2053@gmail.com>
2018-12-03 17:04:26 +0545

    exercise 1

---
+++ machine-learning-ex1/ex1/computeCost.m
@@ -0,0 +1,27 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+t=theta;
+h=X*t;
+h=h-y;
+h=h.^2;
+r=sum(h);
+r=r/(2*m);
+% You need to return the following variables correctly 
+J = r;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+
+
+
+
+% =========================================================================
+
+end
